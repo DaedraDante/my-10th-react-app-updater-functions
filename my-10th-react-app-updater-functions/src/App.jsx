b892df1b5a6fec76978f2ee0fbac883836faf524
DaedraDante <153109749+DaedraDante@users.noreply.github.com>
2025-03-12 20:19:36 +0200

    Update App.jsx

--- my-10th-react-app-updater-functions/src/App.jsx
+++ my-10th-react-app-updater-functions/src/App.jsx
@@ -3,8 +3,6 @@ import Component from "./Component"
 //Updater functions are a function  passed as an argument to  setState() usually
 //ex SetYear(arrow function)
 function App() {
-
-
   return (
     <>
       <Component/>
